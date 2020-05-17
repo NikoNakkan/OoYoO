08cce8ae16c2de996757e74c988187a526875e6a
ksyro98 <ksyro98@protonmail.com>
2020-05-17 15:15:06 +0300

    Merge branch 'master' of /home/kon/Documents/OoYoO with conflicts.

--- Project-code/ooYoo/app/src/main/java/com/softeng/ooyoo/chat/Message.kt
+++ Project-code/ooYoo/app/src/main/java/com/softeng/ooyoo/chat/Message.kt
@@ -1,8 +1,3 @@
 package com.softeng.ooyoo.chat
 
-class Message(private val senderId: String="",
-              private val receiverId: String="",
-              private val message: String="",
-              private val timestamp: String="",
-              private val read: Boolean=false,
-              private val messageId: String="")
+class Message(private val senderId: String, private val receiverId: String, private val message: String, private val timestamp: Long, private val read: Boolean, private val messageId: String)
