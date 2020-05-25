4fcfce36fa00c3f7840c90644c9e9d1b285b63ef
ksyro98 <ksyro98@protonmail.com>
2020-05-25 23:33:02 +0300

    Added a chat window UI.

--- Project-code/ooYoo/app/src/main/java/com/softeng/ooyoo/chat/ChatListAdapter.kt
+++ Project-code/ooYoo/app/src/main/java/com/softeng/ooyoo/chat/ChatListAdapter.kt
@@ -1,15 +1,16 @@
 package com.softeng.ooyoo.chat
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.softeng.ooyoo.R
-import com.softeng.ooyoo.toast
 import com.softeng.ooyoo.user.User
 
 class ChatListAdapter(
@@ -49,18 +50,22 @@ class ChatListAdapter(
                 messageText.subSequence(0, 14).toString() + "..."
             }
         }
+
+        holder.chatLayout.setOnClickListener {
+            val intent = Intent(context, ChatActivity::class.java)
+            intent.putExtra(USER_CHAT_EXTRA_NAME, userMap[receiverId])
+            intent.putExtra(CHAT_EXTRA_NAME, chats[position])
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int = chats.size
 
-    class ChatViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), View.OnClickListener{
-        val imageView: ImageView = itemView.findViewById(R.id.chatProfileImageView)
+    class ChatViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val chatLayout: LinearLayout = itemView.findViewById(R.id.chatItemLayout)
+        val imageView: ImageView = itemView.findViewById(R.id.chatItemProfileImageView)
         val userNameTextView: TextView = itemView.findViewById(R.id.chatUserNameTextView)
         val lastMessageTextView: TextView = itemView.findViewById(R.id.lastMessageTextView)
-
-        override fun onClick(v: View?) {
-            v?.context?.toast("User in chat list is clicked.")
-        }
     }
 
 
