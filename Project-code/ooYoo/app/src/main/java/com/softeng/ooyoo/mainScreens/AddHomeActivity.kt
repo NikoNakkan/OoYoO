cb9d376e32ca0aa30520f1d117abb8c507ae6737
LAPTOP-PGAAQKUC\Νικος Νακκας <nikonakkan21@gmail.com>
2020-05-25 17:25:14 +0300

    Added Lots of Ui

---
+++ Project-code/ooYoo/app/src/main/java/com/softeng/ooyoo/mainScreens/AddHomeActivity.kt
@@ -0,0 +1,13 @@
+package com.softeng.ooyoo.mainScreens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.softeng.ooyoo.R
+
+class AddHomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_home)
+    }
+}
